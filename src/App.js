4b35970ddd265d1e33672c5d113430def9f9a07f
Muhammed-Bilal777 <94797862+Muhammed-Bilal777@users.noreply.github.com>
2023-03-18 21:35:03 +0530

    Created Project with error

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 
 import './App.scss';
-import {  BrowserRouter as Router, Link, Route, Routes } from 'react-router-dom';
+import {  BrowserRouter as Router,  Route, Routes } from 'react-router-dom';
 import Layout from './components/Layout';
 import About from './components/About/';
 import Home from './components/Home/';
 import Sidebar from './components/Sidebar';
+import Contact from './components/Contact';
  
 function App() {
   return (
       <>
         <Router>
            <Sidebar />
+           <Layout />
           <Routes >
               <Route path="/"  element={<Home />} />
               <Route path='/about' element={<About /> } /> 
-              <Route path='/layout' element={<Layout /> } />       
+              <Route path='/contact' element={<Contact />} />       
           </Routes>
         </Router>
           
