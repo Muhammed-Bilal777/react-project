7b5559fcc19a2639329cf26542f9682030f492b0
Muhammed-Bilal777 <94797862+Muhammed-Bilal777@users.noreply.github.com>
2023-03-18 14:50:57 +0530

    deployin action

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-         Deploying Github Action
+         lets connect to github pages
       </header>
     </div>
   );
